0e711e4ae008c550ddcda3ba5a458c7b9dfef2d7
Rafaela <23rafa.goncalves@gmail.com>
2026-08-19 18:15:24 +0100

    Localizadores

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -29,7 +29,7 @@ test('Como utilizar Locator', async ({ page }) => {
 
   // by Class value (full) class tbm é um atributo
   await page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
-  
+
   // by combine differents locators - Add one by one without any blank space
   await page.locator('input[placeholder="Email"]')
   await page.locator('input#exampleInputPassword1')
@@ -43,5 +43,49 @@ test('Como utilizar Locator', async ({ page }) => {
 
   // by exact text match
   await page.locator(':text-is("Basic form")')
+
+})
+
+test('User facing locators', async ({ page }) => {
+
+  await page.goto('http://localhost:4200/pages/iot-dashboard')
+  await page.getByText('Forms').click()
+  await page.getByText('Form Layouts').click()
+
+
+  // Best pratice is try to always use this way to locate elements
+
+  // input field - 
+  await page.getByRole('textbox', { name: 'Email' })
+
+
+  // button
+  await page.getByRole('button', { name: 'Submit' })
+
+
+
+  // by label - this label is placed above the locator that you wanna save
+  await page.getByLabel('Email')
+
+  // Placeholder
+  await page.getByPlaceholder('First Name')
+
+
+  // by title - is an attribute
+  await page.getByTitle('Datepicker')
+
+
+  // by TestId - this is an attribute config with focus to the script automation. Into the dev conde: data-testid="aut"
+})
+
+test('utilizando expect', async ({ page }) => {
+
+  await page.goto('http://localhost:4200/pages/iot-dashboard')
+  await page.getByText('Forms').click()
+  await page.getByText('Form Layouts').click()
+
+  const locator = page.locator('nb-card', { hasText: 'Using the Grid' }).locator('nb-card-header')
+
+  await expect(locator).toHaveText('Using the Grid')
 
 })
